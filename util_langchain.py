d5c6a016a26cf59b81f55ddb0d978862e2c3b96d
xieb03 <264531057@qq.com>
2024-06-20 20:37:44 +0800

    little update

--- util_langchain.py
+++ util_langchain.py
@@ -1,4 +1,5 @@
 # 安装 langchain 后要额外安装 pip install pymupdf rapidocr-onnxruntime
+from langchain.embeddings.base import Embeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyMuPDFLoader
 # 要额外安装 pip install unstructured
@@ -6,8 +7,48 @@ from langchain_community.document_loaders import PyMuPDFLoader
 # Please use the NLTK Downloader to obtain the resource: nltk.download('punkt')，需要魔术方法，下载到 C:\Users\admin\AppData\Roaming\nltk_data 中
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
 from langchain_core.documents.base import Document
+from transformers import AutoModel, AutoTokenizer
 
-from util import *
+from util_torch import *
+
+
+class LangchainEmbeddings(Embeddings):
+    def __init__(self, embedding_model_path=BGE_LARGE_CN_model_dir, max_length=1024, using_gpu=True):
+        self.embedding_tokenizer = AutoTokenizer.from_pretrained(
+            pretrained_model_name_or_path=embedding_model_path,
+            trust_remote_code=True)
+        self.embedding_model = AutoModel.from_pretrained(pretrained_model_name_or_path=embedding_model_path,
+                                                         trust_remote_code=True)
+        self.using_gpu = using_gpu
+        if self.using_gpu:
+            self.embedding_model = self.embedding_model.cuda()
+        self.max_length = max_length
+
+    def embed_query(self, text: str) -> List[float]:
+        encoded_input = self.embedding_tokenizer(text, padding=True, truncation=True, max_length=self.max_length,
+                                                 return_tensors='pt')
+        if self.using_gpu:
+            change_dict_value_to_gpu(encoded_input)
+
+        with torch.no_grad():
+            model_output = self.embedding_model(**encoded_input)
+            # torch.Size([1, n, 1024])
+            # print(model_output[0].shape)
+
+            # Perform pooling. In this case, cls pooling.
+            # cls-pooling：直接取 [CLS] 的 embedding
+            # mean-pooling：取每个 Token 的平均 embedding
+            # max-pooling：对得到的每个 Embedding 取 max
+            # # torch.Size([1, 1024])
+            # sentence_embeddings = model_output[0][:, 0]
+            # torch.Size([1024])，因为要求返回的是一维 list
+            sentence_embeddings = model_output[0][0, 0]
+
+        # normalize embeddings
+        return torch.nn.functional.normalize(sentence_embeddings, p=2, dim=0).tolist()
+
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        return [self.embed_query(text) for text in texts]
 
 
 # 获取 pdfs 逐页数据，可以选择是否对图片解析，注意是解析图片中的文字，而不返回图片：extract_from_images_with_rapidocr
@@ -98,14 +139,26 @@ def check_get_md_pages():
           sep="\n------\n")
 
 
+def check_langchain_embeddings():
+    langchain_embeddings = LangchainEmbeddings()
+    embedding_list = langchain_embeddings.embed_documents(["样例数据-1", "样例数据-2", "错例数据-223"])
+    # 1024 [0.001481, 0.01648, -0.028145, 0.025962, 0.012677]
+    # 1024 [0.015082, 0.004146, -0.015681, 0.03677, 0.017891]
+    # 1024 [0.000608, 0.016712, -0.019185, 0.022082, 0.03458]
+    for embedding in embedding_list:
+        print(len(embedding), list(map(get_round_6, embedding[:5])))
+
+
 @func_timer(arg=True)
 def main():
     assert_equal(len("\n"), 1)
     assert_equal(len("\n\n"), 2)
     assert_equal(len("  \n \n"), 5)
 
-    check_get_pdf_pages()
-    check_get_md_pages()
+    # check_get_pdf_pages()
+    # check_get_md_pages()
+
+    check_langchain_embeddings()
 
 
 if __name__ == '__main__':
